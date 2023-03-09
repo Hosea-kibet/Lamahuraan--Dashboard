4ac7e3e79db78ac6a246ff4fc3f824ca1b01136c
Hosea-kibet <kibethosea6@gmail.com>
2023-03-09 05:15:03 +0300

    return back home link

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,34 +1,39 @@
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";  
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import { Link } from "react-router-dom";
 
-
 const Sidebar = () => {
-
   return (
     <div className="sidebar">
-      <div className="top">
-        <Link to="/" style={{ textDecoration: "none" }}>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <div className="top">
           <span className="logo">lamahuraan </span>
-        </Link>
-      </div>
+        </div>
+      </Link>
       <hr />
       <div className="center">
         <ul>
-          <p className="title">MAIN</p>
-          <li>
-            <DashboardIcon className="icon" />
-            <span>Dashboard</span>
-          </li>
-          <Link to="https://simplesms.co.ke:2121/login.xhtml" style={{ textDecoration: "none" }} target="_blank" rel="noopener noreferrer">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <p className="title">MAIN</p>
+            <li>
+              <DashboardIcon className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </Link>
+          <Link
+            to="https://simplesms.co.ke:2121/login.xhtml"
+            style={{ textDecoration: "none" }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <li>
               <PersonOutlineIcon className="icon" />
               <span>Simple Sms</span>
             </li>
           </Link>
-          
+
           <li>
             <ExitToAppIcon className="icon" />
             <span>Logout</span>
