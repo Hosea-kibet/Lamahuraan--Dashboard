11bb452626bbd0ad04af672f45cbb76c26c61c9d
Hosea-kibet <kibethosea6@gmail.com>
2023-03-10 00:48:42 +0300

    login page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,6 +2,7 @@ import "./list.scss"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Navbar from "../../components/navbar/Navbar"
 import Datatable from "../../components/datatable/Datatable"
+import { Link } from "react-router-dom";
 
 const List = () => {
   return (
@@ -9,6 +10,12 @@ const List = () => {
       <Sidebar/>
       <div className="listContainer">
         <Navbar/>
+        <div className="datatableTitle">
+        Add New Tune
+        <Link to="/skizatunes/new" className="link">
+          Upload
+        </Link>
+      </div>
         <Datatable/>
       </div>
     </div>
